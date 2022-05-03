f7fdfb33f1dddc3bb8805cc1c814188542fbdb17
williamrolando88 <william.rolando88@hotmail.com>
2022-05-03 14:31:00 -0500

    Add: Random greeting selector

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :greetings, default: (:json), only: [:show, :index]
+  get 'greetings/random'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
