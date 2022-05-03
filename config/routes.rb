0acac0a8830448a71225b7c3ab3c71fbba73403e
williamrolando88 <william.rolando88@hotmail.com>
2022-05-03 10:32:20 -0500

    Add: Routes restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :greetings
+  resources :greetings, default: (:json), only: [:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
